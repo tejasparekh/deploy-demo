c576b5282c885b4498d129a08bbfe985cbff053c
Tejas Parekh <tejas@lumiumdesign.com>
2020-04-15 17:55:58 +0530

    Updates

---
+++ precache-manifest.0d66414bf2e67787982f0cf014a88cca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a2632b1bfd4873e1883044eb6876ec",
+    "url": "/deploy-demo/index.html"
+  },
+  {
+    "revision": "70a1870ffe5b2a808bfc",
+    "url": "/deploy-demo/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "76457a9d67e884755343",
+    "url": "/deploy-demo/static/js/2.a54f0d65.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/deploy-demo/static/js/2.a54f0d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a1870ffe5b2a808bfc",
+    "url": "/deploy-demo/static/js/main.a4bfd737.chunk.js"
+  },
+  {
+    "revision": "0b5256849ba168b2d126",
+    "url": "/deploy-demo/static/js/runtime-main.165a46cf.js"
+  }
+]);
